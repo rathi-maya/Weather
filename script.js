e160d207cbcbbeac1d67300636ccd41b7427227a
RATHI M <rathim24it@psnacet.edu.in>
2026-01-01 23:08:48 +0530

    Add files via upload
    Ready to Launch

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = "5e5f82727f2f94ba18d4e710a03e12fb";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data)
+    document.querySelector(".city").textContent = data.name;
+    document.querySelector(".temperature").textContent = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").textContent = data.main.humidity +"%";
+    document.querySelector(".wind").textContent = data.wind.speed + "km/h";
+   const weatherMain = data.weather[0].main.toLowerCase();
+    const iconElement = document.querySelector(".weatherIcon");
+
+    if (weatherMain.includes("clear")) {
+        iconElement.src = "images/clear.png";
+    } else if (weatherMain.includes("cloud")) {
+        iconElement.src = "images/clouds.png";
+    } else if (weatherMain.includes("rain")) {
+        iconElement.src = "images/rain.png";
+    } else if (weatherMain.includes("snow")) {
+        iconElement.src = "images/snow.png";
+    } else if (weatherMain.includes("mist")) {
+        iconElement.src = "images/mist.png";
+    } else {
+        iconElement.src = "images/default.png";
+    }
+}
+
+
+
+
+
+
